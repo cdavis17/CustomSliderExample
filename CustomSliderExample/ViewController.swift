543cc3eff79e8f25c6a37c894c9604b32f920aea
Chris Davis <chris@d2businessalliance.com>
2016-01-24 09:57:29 -0500

    Initial Commit

---
+++ CustomSliderExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomSliderExample
+//
+//  Created by Christopher Davis on 1/24/16.
+//  Copyright © 2016 D2 Business Alliance, LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
